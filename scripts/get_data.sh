1bc74120fff86350084bc569b69326361a1334f0
Dhiranjit <dhiranjitdaimary@gmail.com>
2026-03-17 23:38:20 +0530

    Add get_data script for dataset download

---
+++ scripts/get_data.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+DATA_DIR="data/raw"
+DATA_FILE="$DATA_DIR/arxiv/arxiv-metadata-oai-snapshot.json"
+
+if [ -f "$DATA_FILE" ]; then
+    echo "Dataset already exists. Skipping download."
+else
+    mkdir -p $DATA_DIR
+
+    kaggle datasets download Cornell-University/arxiv \
+        -p $DATA_DIR \
+        --unzip
+
+    echo "Dataset downloaded."
+fi
